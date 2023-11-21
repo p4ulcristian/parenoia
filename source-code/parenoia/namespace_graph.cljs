90cee05fa20c1c380c7747ee7b85bedd099c917d
Paul Cristian <paul931224@gmail.com>
2023-11-21 17:08:19 +0100

    Searchable graph to draw a new one.

--- source-code/parenoia/namespace_graph.cljs
+++ source-code/parenoia/namespace_graph.cljs
@@ -6,24 +6,58 @@
             [parenoia.refactor :as refactor]
             [re-frame.core :refer [dispatch subscribe]]))
 
+
+
+(def depth-one-color   "lightgreen")
+(def depth-two-color   "pink")
+(def depth-three-color "orange")
+(def depth-four-color  "lightblue")
+(def depth-five-color  "blue")
+
+(defn split-ns [ns-name]
+  (clojure.string/split  ns-name #"\."))
+
+(defn ns-name->ns-depth [ns-name]
+  (count (split-ns ns-name)))
+
+(def wizard-color "pink")
+(def backend-color "orange")
+(def extensions-color "lightgreen")
+
+(defn ns-name->first-name [ns-name]
+   (first (split-ns ns-name)))
+
+
+(defn ns-name->ns-color [ns-name]
+  (case (ns-name->first-name ns-name)
+    "wizard" wizard-color 
+    "backend" backend-color 
+    "extensions" extensions-color
+    "lightblue"))
+
 (defn generate-node [file-path id]
   {:id id
-   :label id
+   :label (str (clojure.string/replace (str id) #"\." " |"))
+   :widthConstraint 200
+   :shape "box"
+   :group (ns-name->first-name ns-name)
    :file-path file-path
-   :margin {:top 40,
-            :right 40,
-            :bottom 40,
-            :left 40}
-   :color {:background "lightgreen",
+   :font {:size 18}
+   :margin 10
+   :color {:background (ns-name->ns-color id)
            :border "lightgreen"
-           :hightlight {:color "red"}}})
+           :highlight  {:background "yellow"}
+           :hover      {:background "yellow"}}})
 
 (defn generate-edge [source-id target-id]
   {:from source-id
    :to target-id
    :arrows "to"
-   :color {:color "lightblue"
-           :hightlight "#848484"}})
+   ;:smooth true
+   :color {:color (ns-name->ns-color source-id)
+           :highlight "yellow"}})
+           ;:highlight  {:background "red"}}})
+  
 
 (defn generate-edges [ids target-id]
   (mapv (fn [source-id] (generate-edge source-id target-id))
@@ -31,7 +65,7 @@
 
 (def options
   {:physics {:enabled true
-             :hierarchicalRepulsion {:nodeDistance 300}}
+             :hierarchicalRepulsion {:nodeDistance 500}}
    :layout {:hierarchical {:enabled true
                            :shakeTowards "roots"
                            :direction "UD"
@@ -55,14 +89,14 @@
                     (get-node-with-id nodes node-clicked)]))))})
 
 (defn vis-js-component [nodes edges]
-  (let [ref (react/useRef)
+  (let [[vis set-vis]   (react/useState nil)
         graph {:nodes nodes
                :edges edges}]
-    [:div {:ref ref
-           :style {:height "100vh"
+    [:div {:style {:height "100vh"
                    :width "100vw"
                    :border-radius "10px"}}
-     [:> Graph {:graph graph
+     [:> Graph {:vis (fn [e] (set-vis e))
+                :graph graph
                 :options options
                 :events (events nodes)}]]))
 
@@ -75,6 +109,7 @@
                    :backdrop-filter (if project-map? "blur(10px)" "none")
                    :filter (if project-map? "none" "blur(10px)")
                    :z-index (if project-map? 15 7)}}
+     
      content]))
 
 (defn get-all-edges [files]
@@ -90,17 +125,38 @@
   (mapv (fn [[file-path file]] (generate-node file-path (refactor/get-ns file)))
     files))
 
+
+(defn filter-nodes-by-string [nodes filter-string] 
+     (filter 
+       (fn [{:keys [id]}] (clojure.string/includes? (str id) filter-string))
+       nodes))                      
+
+(defn filter-edges-by-string [edges filter-string]
+ (filter 
+       (fn [{:keys [from to]}] 
+         (and
+          (clojure.string/includes? (str from) filter-string)
+          (clojure.string/includes? (str to)   filter-string)))
+       edges))
+  
+
 (defn view []
   (let [ref (react/useRef)
         files @(subscribe [:db/get [:parenoia :project-last-saved]])
         [nodes set-nodes] (react/useState [])
-        [edges set-edges] (react/useState [])]
+        [edges set-edges] (react/useState [])
+        [local-string set-local-string] (react/useState "")
+        [filter-string set-filter-string]     (react/useState "")]
     (react/useEffect
       (fn []
-        (set-nodes (get-all-nodes files))
-        (set-edges (get-all-edges files))
+        (set-nodes (filter-nodes-by-string (get-all-nodes files) filter-string))
+        (set-edges (filter-edges-by-string (get-all-edges files) filter-string))
         (fn []))
-      #js [files])
+      #js [files filter-string])
 
     [view-wrapper
-     [vis-js-component nodes edges]]))
+     [:<>
+      [:input {:value local-string
+               :on-change #(set-local-string (-> % .-target .-value))
+               :on-blur   #(set-filter-string (-> % .-target .-value))}] 
+      [vis-js-component nodes edges]]]))
